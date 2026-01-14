602eff22c4a6465ffe4c76d938c576f8f538a164
Oleksandr Rura <115620336+chadowgg@users.noreply.github.com>
2026-01-14 15:41:38 +0200

    challenge-arrays

---
+++ challenge-arrays.ts
@@ -0,0 +1,7 @@
+let temperatures: number[] = [20, 25, 30];
+// temperatures.push('hot');
+
+let colors: string[] = ["red", "green", "blue"];
+// colors.push(true);
+
+let mixedArray:(number | string)[] = [1, 'two,', 3, 'green', 'blue'];
