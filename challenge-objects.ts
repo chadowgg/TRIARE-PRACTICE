49ea8e0392f42576664393b4000329706fde5162
Oleksandr Rura <115620336+chadowgg@users.noreply.github.com>
2026-01-14 16:06:17 +0200

    challenge-objects

---
+++ challenge-objects.ts
@@ -0,0 +1,11 @@
+let bike: { brand: string; year: number} = { brand:'honda', year: 1994};
+// bike.year = 'old';
+
+let laptop: { brand: string; year: number } = { brand: 'asus', year: 2021 };
+// let laptop2: { brand: string; year: number } = { brand: 'lenovo'};
+
+let product1 = { title: 'shirt', price: 20};
+let product2 = { title: 'pants' };
+let products: { title: string; price?: number }[] = [product1, product2];
+
+// products.push({ title: 'shoes', price: 'expensive'});
