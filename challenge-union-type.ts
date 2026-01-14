8b5ae2f510e5b46cea2c8011533d41c28ab01fa7
Oleksandr Rura <115620336+chadowgg@users.noreply.github.com>
2026-01-14 15:31:00 +0200

    Challenge Union Type

---
+++ challenge-union-type.ts
@@ -0,0 +1,8 @@
+let orderStatus: 'processing' | 'shipped' | 'delivered' = 'processing';
+orderStatus = 'shipped';
+orderStatus = 'delivered';
+
+let discount: number | string = 20;
+discount = '20%';
+
+
