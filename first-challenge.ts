94ad1c1279c821b37ed84635d25a13b0d093c0c0
Oleksandr Rura <115620336+chadowgg@users.noreply.github.com>
2026-01-14 15:03:17 +0200

    first-challenge

---
+++ first-challenge.ts
@@ -0,0 +1,11 @@
+let greeting: string = 'Hello';
+greeting = greeting.toUpperCase();
+
+let age: number = 25;
+age += 5;
+
+let isAdult: boolean = age >= 18;
+console.log(isAdult);
+isAdult = !isAdult;
+console.log(isAdult);
+
